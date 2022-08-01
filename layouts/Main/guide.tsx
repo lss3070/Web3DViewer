5242b657c553a5ef06bde8124e8b684ee0e8d4bc
karatej <karatej@card.cafe24app.com>
2022-08-01 13:53:26 +0900

    Change env url to string url

--- layouts/Main/guide.tsx
+++ layouts/Main/guide.tsx
@@ -7,7 +7,7 @@ const Guide=()=>{
     const {commonState,setFiltPath}=useCommonSWR();
 
     const openTree=()=>{
-        fetch(process.env.NEXT_PUBLIC_ENV_TREE!).then(async(file)=>{
+        fetch('https://web3dviewer_worker.lss3070.workers.dev/tree/tree.obj').then(async(file)=>{
             return await file.blob()
         }).then((blob)=>{
             
@@ -26,7 +26,7 @@ const Guide=()=>{
     }
 
     const openHelmet=()=>{
-        fetch(process.env.NEXT_PUBLIC_ENV_HELMET!).then(async(file)=>{
+        fetch('https://web3dviewer_worker.lss3070.workers.dev/helmet/DamagedHelmet.glb').then(async(file)=>{
             return await file.blob()
         }).then((blob)=>{
             
@@ -42,57 +42,10 @@ const Guide=()=>{
                 fileMap:fileMap!
             })
         })
-    //    Promise.all([
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_BIN!),
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_GLTF!),
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_ALBEDO!),
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_AO!),
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_EMISSIVE!),
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_METALROUGHNESS!),
-    //         fetch(process.env.NEXT_PUBLIC_ENV_TREE_HELMET_NORMAL!)
-    //     ]).then(async(result)=>{
-    //         return [
-    //             await result[0].blob(),
-    //             await result[1].blob(),
-    //             await result[2].blob(),
-    //             await result[3].blob(),
-    //             await result[4].blob(),
-    //             await result[5].blob(),
-    //             await result[6].blob()
-    //         ]
-    //     }).then(result=>{
-    //         console.log(result);
-
-    //         const fileMap = new Map<string,File>();
-     
-    //         fileMap.set('DamagedHelmet.bin'
-    //         ,new File([result[0]],'DamagedHelmet.bin'));
-    //         fileMap.set('DamagedHelmet.gltf'
-    //         ,new File([result[1]],'DamagedHelmet.gltf'));
-    //         fileMap.set('Default_albedo.jpg'
-    //         ,new File([result[2]],'Default_albedo.jpg'));
-    //         fileMap.set('Default_AO.jpg'
-    //         ,new File([result[3]],'Default_AO.jpg'));
-    //         fileMap.set('Default_emissive.jpg'
-    //         ,new File([result[4]],'Default_emissive.jpg'));
-    //         fileMap.set('Default_metalRoughness.jpg'
-    //         ,new File([result[5]],'Default_metalRoughness.jpg'));
-    //         fileMap.set('Default_normal.jpg'
-    //         ,new File([result[6]],'Default_normal.jpg'));
-
-    //         const objectURL= URL.createObjectURL(result[1]);
-
-    //         setFiltPath({
-    //             originPath:objectURL!,
-    //             originExtension:'gltf',
-    //             originName:'helmet',
-    //             fileMap:fileMap!
-    //         })
-    //     })
     }
 
     const openMannequin=()=>{
-        fetch(process.env.NEXT_PUBLIC_ENV_MANNEQUIN!).then(async(file)=>{
+        fetch('https://web3dviewer_worker.lss3070.workers.dev/mannequin/Samba_Dancing.fbx').then(async(file)=>{
             return await file.blob()
         }).then((blob)=>{
             
