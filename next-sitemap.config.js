5f671bdaa4819bd3adc2af904796cbafd34aa9b5
karatej <karatej@card.cafe24app.com>
2022-09-13 19:19:44 +0900

    Update Config info

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl:process.env.SITE_URL,
+    siteUrl:process.env.SITE_URL||'https://www.web3dviewer.net/',
     generateRobotsTxt: true,
   };
   
